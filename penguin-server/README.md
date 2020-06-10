515139ae19a0195a7ec007a7545fe8053afb34b8
tqboss <907766352@qq.com>
2020-06-10 18:00:28 +0800

    开启session

--- penguin-server/README.md
+++ penguin-server/README.md
@@ -1,2 +1,10 @@
-# 环境要求
-PHP7.1+
+# penguin-server
+
+## 部署方式 
+暂以模块形式部署到Apache服务器中
+
+## 环境要求
++ Apache 2.4
++ PHP 7.1+
++ Workerman 3
++ ThinkPHP 6+
--- penguin-server/config/session.php
+++ penguin-server/config/session.php
@@ -7,7 +7,7 @@ return [
     // session name
     'name'           => 'PHPSESSID',
     // SESSION_ID的提交变量,解决flash上传跨域
-    'var_session_id' => '',
+    'var_session_id' => 'penguin_session_id',
     // 驱动方式 支持file cache
     'type'           => 'file',
     // 存储连接标识 当type使用cache的时候有效
